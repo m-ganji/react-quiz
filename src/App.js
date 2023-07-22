7230cd77424a0abfb1dd9481cba0b233e710dccb
mohamad ganji <mohamad.ganjif@gmail.com>
2023-07-22 15:50:12 +0330

    first commit

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import "./App.css";
+import "./index.css";
+import Header from "./Header";
+import Main from "./Main";
+import { useEffect, useReducer } from "react";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
+import Question from "./Question";
+
+const initialState = {
+  questions: [],
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataRecieved":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+    default:
+      throw new Error("Action unknown");
+  }
+}
+
+function App() {
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+
+  console.log(questions);
+
+  useEffect(() => {
+    fetch("http://localhost:9000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataRecieved", payload: data }))
+      .catch((error) => dispatch({ type: "dataFailed" }));
+  }, []);
+
+  return (
+    <div className="app">
+      <Header />
+      <Main>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && <StartScreen questions={questions.length} />}
+      </Main>
+    </div>
+  );
+}
+
+export default App;
